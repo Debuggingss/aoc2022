f6afa494eb6cf0f90934593efeb220a04ef6d21a
Debuggingss <55459283+Debuggingss@users.noreply.github.com>
2022-12-06 16:49:55 +0100

    Add Day 6

---
+++ src/main/kotlin/dev/debuggings/aoc2022/days/Day6.kt
@@ -0,0 +1,40 @@
+package dev.debuggings.aoc2022.days
+
+object Day6 : Day(6) {
+
+    override fun part1() {
+        val data = getInput()
+
+        val buffer = mutableListOf<String>()
+        var processed = 0
+
+        data.split("").drop(1).forEachIndexed { i, c ->
+            if (processed != 0) return@forEachIndexed
+
+            buffer.add(c)
+
+            if (buffer.distinct().size == 4) processed = i + 1
+            if (buffer.size == 4) buffer.removeFirst()
+        }
+
+        println("\nAnswer: $processed")
+    }
+
+    override fun part2() {
+        val data = getInput()
+
+        val buffer = mutableListOf<String>()
+        var processed = 0
+
+        data.split("").drop(1).forEachIndexed { i, c ->
+            if (processed != 0) return@forEachIndexed
+
+            buffer.add(c)
+
+            if (buffer.distinct().size == 14) processed = i + 1
+            if (buffer.size == 14) buffer.removeFirst()
+        }
+
+        println("\nAnswer: $processed")
+    }
+}
